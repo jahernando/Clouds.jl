4d6d6301b874da05285f72fe4ec1ad2cbb1eda96
jahernando <hernando.joseangel@gmail.com>
2022-10-14 13:10:36 +0200

    line in sources

--- src/sources.jl
+++ src/sources.jl
@@ -114,6 +114,10 @@ end
 returns 3D or 2D points in a line smeared with a gaussian
 
 """
+
+"""
+Just a smeared line!
+"""
 function line(;ndim = 3, threshold = 0.)
 
 	tstep = 0.1
@@ -153,3 +157,42 @@ function line(;ndim = 3, threshold = 0.)
 	return (coors = coors, cells = cells, contents = contents, edges = edges)
 
 end
+# function line(;ndim = 3, threshold = 0.)
+#
+# 	tstep = 0.1
+# 	ts = 0:tstep:1.
+# 	ax, bx, cx = 5., 0., 0.
+# 	ay, by, cy = -5., -5., 0.
+# 	az, bz, cz = 5., -5., 0.
+# 	xx = cx .* ts .* ts + ax .* ts .+ bx
+# 	yy = cy .* ts .* ts + ay .* ts .+ by
+# 	zz = cz .* ts .* ts + az .* ts .+ bz
+#
+# 	zsig  = 5.
+# 	sigma = 2 * tstep #* ma(ax, ay, az)
+# 	xxbins = minimum(xx) - zsig .* sigma : sigma : maximum(xx) + zsig .*sigma
+# 	yybins = minimum(yy) - zsig .* sigma : sigma : maximum(yy) + zsig .*sigma
+# 	zzbins = minimum(zz) - zsig .* sigma : sigma : maximum(zz) + zsig .*sigma
+#
+# 	heigth  = 1000
+# 	xxcontents = heigth * ones(Base.size(xx))
+#
+# 	coors = ndim == 2 ? (xx, yy) : (xx, yy, zz)
+# 	edges = ndim == 2 ? (xxbins, yybins) : (xxbins, yybins, zzbins)
+# 	hh  = SB.fit(SB.Histogram, coors, SB.weights(xxcontents), edges)
+#
+# 	centers = [(edge[2:end] + edge[1:end-1])./2 for edge in edges]
+#
+# 	factor = 10.
+# 	sigma_kernel = (sigma * factor) .* ones(ndim)
+# 	weights_ = IF.imfilter(hh.weights, IF.Kernel.gaussian(sigma_kernel))
+#
+# 	cells    = findall(x -> x .> threshold, weights_)
+# 	coors    = [[centers[i][cell[i]] for cell in cells] for i in 1:1:ndim]
+# 	contents = [hh.weights[cell] for cell in cells]
+#
+# 	contents = [weights_[cell] for cell in cells]
+#
+# 	return (coors = coors, cells = cells, contents = contents, edges = edges)
+#
+# end
